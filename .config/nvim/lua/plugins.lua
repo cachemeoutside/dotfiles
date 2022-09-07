caa8a29e21a7f43f23f8284de3fa1e0b7cd5be36
Anthony Vuong <vuonga91@gmail.com>
2022-09-06 18:07:34 -0700

    Add .config contents

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,31 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'sainnhe/sonokai'
+  use 'ellisonleao/glow.nvim'
+  use 'ojroques/nvim-hardline'
+  use 'neovim/nvim-lspconfig'
+  use {
+   'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+  'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
